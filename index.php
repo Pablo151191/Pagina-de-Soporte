1f87cbdeda6d4f280fd65edc7641b2fa12b94d8a
Pablo <pablojimenezb24@gmail.com>
2024-06-20 20:39:44 -0600

    Pagina de Soporte

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once "./parte_superior.php" ?>
+
+
+<div class="titulo">
+    <p>Le ofrecemos un servicio de soporte que lo ayudará a mantener su equipo en buen estado</p>
+
+</div>
+
+<body>
+    <div class="main-img">
+        <img src="./images/portada.jpg" alt="">
+
+    </div>
+</body>
+
+
+<?php require_once "./parte_Inferior.php" ?>
---
+++ parte_superior.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Pagina de Soporte" content="Pagina de prueba">
+
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php?">
+            <img src="./images/icono.png" alt="">
+        </a>
+    </div>
+
+    <div class="menu-container">
+        <div class="menu-derecha">
+            <a class="enlaces-menu-derecha" href="https://google.com">Informacion de la Pagina</a>
+            <a class="enlaces-menu-derecha" href="https://google.com">Contactenos</a>
+        </div>
+        <div class="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</menu>
+
+</html>
