f1726cbbbb3f12697367aa64e297f512666b50aa
Pablo <pablojimenezb24@gmail.com>
2024-07-29 20:25:01 -0600

    Cards

--- body_cards.php
+++ body_cards.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Flex Card Hover Slider</title>
+    <title>Pagina de Soporte</title>
     <link rel="stylesheet" href="./styles/main.css">
 </head>
 <body>
--- parte_superior.php
+++ parte_superior.php
@@ -17,8 +17,9 @@
 
     <div class="menu-container">
         <div class="menu-derecha">
-            <a class="enlaces-menu-derecha" href="https://google.com">Informacion de la Pagina</a>
-            <a class="enlaces-menu-derecha" href="https://google.com">Contactenos</a>
+            <a class="enlaces-menu-derecha" href="https://maps.app.goo.gl/CK7bKQkqqwTsRg9e8"><img src="./images/mapa.png" style="widh: 40px; height: 40px;"></a>
+            <a class="enlaces-menu-derecha" href="https://wa.me/50670408239"> <img src="./images/whatsapp-fill.svg" style="widh: 40px; height: 40px;"></a>
+            <a class="enlaces-menu-derecha" href="https://www.facebook.com/profile.php?id=100095116016726&mibextid=ZbWKwL"> <img src="./images/facebook-circle-fill.svg" alt="Facebook" style="widh: 40px; height: 40px;"></a>
         </div>
         <div class="hamburger-icon">
             <span></span>
@@ -26,6 +27,7 @@
             <span></span>
         </div>
     </div>
+    
 </menu>
 
 </html>
